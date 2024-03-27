24923f7a12da4a432cb313dec50aae43a829e056
HelenYazh <olenka.yazh05@gmail.com>
2024-03-27 21:14:25 +0200

    task5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
         .toString(16)
         .padStart(6, 0)}`;
 }
+
+
+const btn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", handleClick);
+
+
+function handleClick(event) {
+    const randomColor = getRandomHexColor();
+    color.textContent = randomColor;
+    body.style.backgroundColor = randomColor;
+}
